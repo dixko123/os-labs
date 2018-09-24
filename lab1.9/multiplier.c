148d08de6ec2be963f07f3c4174d66e1e4d852b3
dixko123 <42257229+dixko123@users.noreply.github.com>
2018-09-23 22:30:36 -0500

    Update multiplier.c

--- lab1.9/multiplier.c
+++ lab1.9/multiplier.c
@@ -3,10 +3,10 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <unistd.h>
-#include <time.h>
+
 
 #define MATRIX_TOTAL_SIZE    9
-#define ROW_SIZE             3
+#define ROWS             3
 
 
 int NUM_BUFFERS;
@@ -37,24 +37,35 @@ int main(void){
 
 	int i;
 	long *matrixA,*matrixB,*col,*fila;
-	matrixA = readMatrix("testingMatrix.dat");
-	matrixB = readMatrix("testingMatrix2.dat");
-
-	printf("BUFFERS No = ");	
-	scanf("%d",&NUM_BUFFERS);
+	matrixA = readMatrix("testingMatrix.dat");               //CHANGE THIS NAME FILE TO USE YOUR MATRIX 
+	matrixB = readMatrix("testingMatrix2.dat");              //CHANGE THIS NAME FILE TO USE YOUR MATRIX 
 	result = malloc ((long)MATRIX_TOTAL_SIZE*(sizeof(long)));
 	
+	printf("BUFFERS No = ");	
+	scanf("%d",&NUM_BUFFERS);
+	
+	
 	//CREATE THE ARRAY OF ARRAYS long **buffers
-	buffers = (long**)malloc (ROW_SIZE*(sizeof(long*)));
-	for(i=0;i<ROW_SIZE;i++){
-		(buffers+i)=(long) malloc (ROW_SIZE*sizeof(long));
+	buffers = (long**)malloc (NUM_BUFFERS*(sizeof(long*)));
+	for(i=0;i<NUM_BUFFERS;i++){
+		(buffers+i)=(long) malloc (ROWS*sizeof(long));
 	}
 		
 	mutexes = malloc ((long)NUM_BUFFERS*sizeof(pthread_mutex_t));
 	for(i=0;i<NUM_BUFFERS;i++){
 		if(pthread_mutex_init(&mutexes[i],NULL)!=0){printf("\n Mutex init failed\n");return 1;}
 	}
+	/*
+	for(i=0;i<NUM_BUFFERS;i++){
+		(buffers+i)=(long) malloc (ROWS*sizeof(long));
+	}
 		
+	mutexes = malloc ((long)NUM_BUFFERS*sizeof(pthread_mutex_t));
+	for(i=0;i<NUM_BUFFERS;i++){
+		if(pthread_mutex_init(&mutexes[i],NULL)!=0){printf("\n Mutex init failed\n");return 1;}
+	}
+	
+	*/	
 	multiply(matrixA,matrixB);
 
 	if(saveResultMatrix(result)!=0){fprintf(stderr,"Unable to create new file.\n");}
@@ -73,7 +84,7 @@ int main(void){
 	if(saveResultMatrix(result)!=0){fprintf(stderr,"Unable to create new file.\n");}
 	*/
 	for(i=0; i<NUM_BUFFERS;i++){pthread_mutex_destroy(&mutexes[i]);}
-	for(i =0;i<ROW_SIZE;i++){free(buffers[i]);}
+	for(i =0;i<ROWS;i++){free(buffers[i]);}
 	free(buffers);
 	return 0;
 		
@@ -103,9 +114,9 @@ long* readMatrix(char *filename){
 long* getColumn (int col,long *matrix){
 
 	unsigned long i;
-	long myMatrix = malloc (ROW_SIZE sizeof(long));
-	for(i=0;i<ROW_SIZE;i++){
-		myMatrix[i]=matrix[i*ROW_SIZE+col];	
+	long myMatrix = malloc (ROWS sizeof(long));
+	for(i=0;i<ROWS;i++){
+		myMatrix[i]=matrix[i*ROWS+col];	
 		printf("COLUMNS: %d\n",myMatrix[i]);
 	}
 	return myMatrix;
@@ -113,9 +124,9 @@ long* getColumn (int col,long *matrix){
 
 long* getRow(int row, long *matrix){
 	unsigned long i;
-	unsigned long rmatrix =ROW_SIZE*row;
-	long myMatrix = malloc (ROW_SIZE sizeof(long));
-	for(i=0;i<ROW_SIZE;i++){
+	unsigned long rmatrix =ROWS*row;
+	long myMatrix = malloc (ROWS sizeof(long));
+	for(i=0;i<ROWS;i++){
 		myMatrix[i]=matrix[rmatrix++];
 		printf("ROWS : %d\n",myMatrix[i]);
 	} 
@@ -150,7 +161,7 @@ long dotProduct(long *vec1, long *vec2){
 	int i;	
 	long temp;
 	long suma=0;
-	for (i=0;i<ROW_SIZE;i++){
+	for (i=0;i<ROWS;i++){
 		temp=(vec1[i]*vec2[i]);
 		//printf("temp: %d\n",temp);	
 		suma+=temp;
@@ -158,24 +169,34 @@ long dotProduct(long *vec1, long *vec2){
 	return suma;
 }
 long *multiply(long *matA, long *matB){
-	int i,visibleScope;
-  	myStruct bufferThreads[ROW_SIZE];
+	int i,
+	    visibleScope,
+	    rmatrix;
+  	myStruct bufferThreads[ROWS];
 
-	pthread_t tid [ROW_SIZE];
+	pthread_t tid [ROWS];
 	pthread_attr_t attr;
 	pthread_attr_init(&attr);
-	if(pthread_attr_getscope(&attr,&visibleScope)!=0){fprintf(stderr,"UNABLE TO GET SCHEDULER SCOPE\n");}
-	if(pthread_attr_setscope(&attr,PTHREAD_SCOPE_SYSTEM)!=0){printf("Unable to set scheduling policy\n");}
-
-	for(i=0;i<ROW_SIZE;i++){
+	for(i=0;i<ROWS;i++){
 		bufferThreads[i].row=i;
 		bufferThreads[i].matrixA=matA;
 		bufferThreads[i].matrixB=matB;
+		pthread_create(&tid[i],&attr,runner,(void*)&bufferThreads[i]);
+	}
+	/*
 
+	for(i=0;i<ROWS;i++){
+		bufferThreads[i].row=i;
+		bufferThreads[i].matrixA=matA;
+		bufferThreads[i].matrixB=matB;
 		pthread_create(&tid[i],&attr,runner,(void*)&bufferThreads[i]);
 	}
 
-	for(i =0; i<ROW_SIZE;i++){ pthread_join(tid[i],NULL);}
+	*/
+
+	
+
+	for(i =0; i<ROWS;i++){ pthread_join(tid[i],NULL);}
 	return 0;
 }
 
@@ -197,24 +218,24 @@ void *runner(void *param)
 	int i;
 	int j;
 	int availableBuffer; 
+	int count_rows;
 	       //Buffer Satatus
-	myStruct *mR =param; //1
-	int lockBuffer;      //2 
-	lockBuffer = getLock(); //3
+	myStruct *mR =param; 
+	int lockBuffer;      
+	lockBuffer = getLock(); 
 
 	while(-1==lockBuffer){lockBuffer = getLock();sleep(1);}      //Espera un buffer disponible
 	
-	for(i=0; i<ROW_SIZE;i++){
+	for(i=0; i<ROWS;i++){
 	//printf("EL OOOOOOOB");
-	
-	printf("DOT_PRODCUT : %ld\n", dotProduct(getRow(mR->row,mR->matrixA),getColumn(i,mR->matrixB)));
+	//count_row++;
+	//printf("DOT_PRODCUT : %ld\n", dotProduct(getRow(mR->row,mR->matrixA),getColumn(i,mR->matrixB)));
+	//printf("DOT_PRODCUT : %ld\n", dotProduct(getRow(mR->row,mR->matrixA),getColumn(i,mR->matrixB)));
 		buffers[mR->row][i] = dotProduct(getRow(mR->row,mR->matrixA),getColumn(i,mR->matrixB));
+	//buffers[count_row++][i] = dotProduct(getRow(count_row++,mR->matrixA),getColumn(i,mR->matrixB));	
 	}
 	
-	for(j=0; j<ROW_SIZE;j++){
-		
-		result[(mR->row*ROW_SIZE)+j]=buffers[mR->row][j];
-	}
+	for(j=0; j<ROWS;j++){result[(mR->row*ROWS)+j]=buffers[mR->row][j];}
 	releaseLock(lockBuffer);
 	pthread_exit(0);
 }
