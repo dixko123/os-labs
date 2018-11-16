6309381d491f6295d83d434e1f010cdb5d19f338
dixko123 <42257229+dixko123@users.noreply.github.com>
2018-11-16 08:13:22 -0600

    Create server1.c

---
+++ lab1.7/server1.c
@@ -0,0 +1,78 @@
+/*  Make the necessary includes and set up the variables.  */
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <sys/un.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+typedef struct{
+    char operation;
+    int valores[10];
+}package;
+
+int main()
+{
+    int server_sockfd, client_sockfd;
+    int server_len, client_len;
+    struct sockaddr_un server_address;
+    struct sockaddr_un client_address;
+
+/*  Remove any old socket and create an unnamed socket for the server.  */
+
+    unlink("server_socket");
+    server_sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
+
+/*  Name the socket.  */
+
+    server_address.sun_family = AF_UNIX;
+    strcpy(server_address.sun_path, "server_socket");
+    server_len = sizeof(server_address);
+    bind(server_sockfd, (struct sockaddr *)&server_address, server_len);
+
+/*  Create a connection queue and wait for clients.  */
+
+    listen(server_sockfd, 5);
+    while(1) {
+        char ch;
+
+        printf("server waiting\n");
+
+/*  Accept a connection.  */
+
+        client_len = sizeof(client_address);
+        client_sockfd = accept(server_sockfd, 
+            (struct sockaddr *)&client_address, &client_len);
+
+/*  We can now read/write to client on client_sockfd.  */
+	package p;
+
+        read(client_sockfd, &p, sizeof(package));
+	int res=0;
+	int i =0;
+
+	if(p.operation == '+'){
+		for(i=0;i<10;i++){
+			res = res + p.valores[i];
+		}
+	}
+	else if(p.operation =='-'){
+		for(i=0;i<10;i++){
+			res = res - p.valores[i];
+		}
+	}
+	else if(p.operation == '*'){
+		res =1;
+		for(i=0;i<10;i++){
+			res = res*p.valores[i];
+		}
+	}
+	else{
+		printf("invalid character");
+	}
+	printf("Resultado del servidor : %d \n",res);
+        
+        close(client_sockfd);
+    }
+}
